573d3972ff11cf78a2f849801f336bf3109f3bb3
fbaileyjr <frankbaileyjr@gmail.com>
2020-05-06 10:07:16 -0400

    Finished while-loop

---
+++ ex33.py
@@ -0,0 +1,19 @@
+#!/Users/frank.baileyaxonius.com/.pyenv/shims/python
+
+i = 0
+numbers = [] 
+
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Number now: ", numbers)
+
+
+
+print("The numbers: ")
+
+
+for num in numbers:
+    print(num)
